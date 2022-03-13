949793aae13de4effac461dc73e6aa116f559bad
jaxydee <worldneedstester@gmail.com>
2022-03-13 19:32:45 +0530

    acess spcifier is added

---
+++ src/com/access/Info.java
@@ -0,0 +1,11 @@
+package com.access;
+
+public class Info  {
+public static void main(String[] args) {
+	data h=new data();
+	h.hippo();
+	h.rhino();
+	
+	
+}
+}
---
+++ src/com/access/data.java
@@ -0,0 +1,16 @@
+package com.access;
+
+ class data {
+	 protected void hippo() {
+	System.out.println("Hippo means Hippopotamus");
+
+}
+ protected  void rhino() {
+   System.out.println("Rhyno means Rhinosarous");
+
+}
+ public static void main(String[] args) {
+	data h=new data();
+	h.rhino();
+}
+}
